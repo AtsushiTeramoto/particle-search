6edd6af59f643d688adb355c5a2aae0f269e0713
Atsushi Teramoto <19401881+AtsushiTeramoto@users.noreply.github.com>
2018-08-22 22:58:41 +0900

    ツリーの作成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+struct Particle {
+    position: [f64; 3],
+    kernel_h: f64,
+}
+
+enum Tree<'a> {
+    Leaf {
+        bound: [(f64, f64); 3],
+        particle: &'a Particle,
+    },
+    Node {
+        bound: [(f64, f64); 3],
+        child: [Option<Box<Tree<'a>>>; 8],
+    },
+}
+
 fn main() {
     println!("Hello, world!");
 }
